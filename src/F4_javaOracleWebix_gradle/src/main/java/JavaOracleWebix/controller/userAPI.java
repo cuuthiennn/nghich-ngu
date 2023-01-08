516e54eb16d7fc120c30e6671dfcf21821dba95b
cuuthiennn <thienweret@gmail.com>
2023-01-08 14:28:26 +0700

    ThuNghiem

--- src/F4_javaOracleWebix_gradle/src/main/java/JavaOracleWebix/controller/userAPI.java
+++ src/F4_javaOracleWebix_gradle/src/main/java/JavaOracleWebix/controller/userAPI.java
@@ -1,5 +1,6 @@
 package JavaOracleWebix.controller;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import JavaOracleWebix.entity.User;
@@ -29,5 +31,35 @@ public class userAPI {
 			// TODO: handle exception
 		}
 		return ResponseEntity.ok(result);
+	}
+	
+	@GetMapping("/getUserByRoleName")
+	ResponseEntity<?> getUserByRoleName(@RequestParam("roleName") String roleName) throws SQLException{
+		List<User> temp = new ArrayList<>();
+		try {
+			temp = usrServ.getUserByRoleName(roleName);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return ResponseEntity.ok(temp);
+	}
+	
+	@GetMapping("/getUserByRoleNameN")
+	ResponseEntity<?> getUserByRoleNameN(@RequestParam("roleName") String roleName) throws SQLException{
+
+		return ResponseEntity.ok(usrServ.getUserByRoleNameN(roleName));
+	}
+	
+	@GetMapping("/getUserByIsUse")
+	ResponseEntity<?> getUserByIsUse(@RequestParam("isUse") String isUse) throws SQLException{
+		List<User> temp = new ArrayList<>();
+		try {
+			temp = usrServ.getUserByIsUse(isUse);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return ResponseEntity.ok(temp);
 	}
 }
